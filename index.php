c49f145693735dbda963a991078d439d9be07aba
Ahtasham Sandhu <ahtashamsandhu6@gmail.com>
2022-12-14 15:40:05 +0500

    updated

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include_once 'head.php'
+?>
+
+<body>
+<!--Main-->
+<?php
+include_once 'nav.php'
+?>
+
+<div class="sign-in-section">
+    <div class="container">
+        <div class="sign-in-wrap">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-container">
+                            <div class="form-logo">
+                                <img src="assets/img/1IGS-logo.png" alt="">
+                            </div>
+
+                            <h1>Welcome <span>Ahtasham Sandhu</span></h1>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-6 sign-in-img"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+include_once 'footer.php';
+include_once 'script.php';
+?>
+
+
+</body>
+</html>
